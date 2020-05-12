449ed21b03ad5b2a135767ba369d8287dda55132
OM-10 <61705645+OM-10@users.noreply.github.com>
2020-05-12 05:37:01 +0530

    first commit

---
+++ fbsearch.js
@@ -0,0 +1,19 @@
+<input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+<ul id="myMenu">
+  <li><a href="#">HTML</a></li>
+  <li><a href="#">CSS</a></li>
+  <li><a href="#">JavaScript</a></li>
+  <li><a><label class="container">One
+    <input type="checkbox" class="chkbx" value="Z" >
+    <span class="checkmark"><img src="recipe2.jpg" style="height: 30px; width: 30px;"> </span>  
+</label></a></li>
+  <li><a href="#">Python</a></li>
+  <li><a><input type="checkbox" class="chkbx" value="N"/>nmz</a></li>
+  <li><a href="#">SQL</a></li>
+  <li><a href="#">Bootstrap</a></li>
+  <li><a href="#">Node.js</a></li>
+</ul>
+
+
+<textarea type="text" id="selectedtext" placeholder="ingredients name" readonly></textarea> <br>
+    <input type="text" id="count" placeholder="ingredient count" readonly>
